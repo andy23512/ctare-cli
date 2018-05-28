ccf0b18190741d0907439fdd394e350c308e2c67
andy23512 <andy23512@gmail.com>
2018-05-29 00:52:50 +0800

    block no name execution, use absolute path to prevent command alias bug, update browserslist via perl, add fonts to .gitignore, use cp to inject files

--- ctare.sh
+++ ctare.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-vue create $@
-cd $1
-yarn add axios jquery semantic-ui-reset
-yarn add -D awesome-fontmin-loader charactor-scanner imagemin imagemin-gifsicle imagemin-mozjpeg imagemin-pngquant img-loader pug pug-plain-loader 
-echo "\n\n# add by ctare\nsrc/assets/fonts/" >> .gitignore
-git clone https://github.com/andy23512/ctare-cli/
-mv ctare-cli/* .
+if [[ $# -eq 0 ]] ; then
+    \echo 'Usage: ./ctare.sh [project name]'
+    \exit 1
+fi
+\vue create $1
+\cd $1
+\yarn add axios jquery semantic-ui-reset
+\yarn add -D awesome-fontmin-loader charactor-scanner imagemin imagemin-gifsicle imagemin-mozjpeg imagemin-pngquant img-loader pug pug-plain-loader
+\perl -0 -i -pe 's/"browserslist": \[.*?\]/"browserslist": [\n    "> 1% in TW",\n    "last 3 versions",\n    "not ie <= 11"\n  ]/s' package.json
+\echo "\n# add by ctare\nsrc/assets/fonts/" >> .gitignore
+\git clone https://github.com/andy23512/ctare-cli/
+\cp -rf ctare-cli/* .
+\rm -rf .git ctare-cli ctare.sh
