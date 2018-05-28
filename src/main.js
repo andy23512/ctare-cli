282b520cce94816103b8783c2de68a3264af8823
andy23512 <andy23512@gmail.com>
2018-05-29 00:58:43 +0800

    use @ instead of . in main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import '@/assets/global.sass'
 import Vue from 'vue'
 import axios from 'axios'
 
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
