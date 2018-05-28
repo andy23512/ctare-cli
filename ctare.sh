ed32eb1f7a8958832d547d26d337a5172fb759a9
andy23512 <andy23512@gmail.com>
2018-05-29 00:53:45 +0800

    change tw to lowercase in browserslist

--- ctare.sh
+++ ctare.sh
@@ -7,7 +7,7 @@ fi
 \cd $1
 \yarn add axios jquery semantic-ui-reset
 \yarn add -D awesome-fontmin-loader charactor-scanner imagemin imagemin-gifsicle imagemin-mozjpeg imagemin-pngquant img-loader pug pug-plain-loader
-\perl -0 -i -pe 's/"browserslist": \[.*?\]/"browserslist": [\n    "> 1% in TW",\n    "last 3 versions",\n    "not ie <= 11"\n  ]/s' package.json
+\perl -0 -i -pe 's/"browserslist": \[.*?\]/"browserslist": [\n    "> 1% in tw",\n    "last 3 versions",\n    "not ie <= 11"\n  ]/s' package.json
 \echo "\n# add by ctare\nsrc/assets/fonts/" >> .gitignore
 \git clone https://github.com/andy23512/ctare-cli/
 \cp -rf ctare-cli/* .
