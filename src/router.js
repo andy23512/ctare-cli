e6a6e98adb84a102b262014764198fdff1376f97
andy23512 <andy23512@gmail.com>
2018-06-07 16:39:01 +0800

    fix router problem

--- src/router.js
+++ src/router.js
@@ -9,8 +9,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '*', redirect: '/'
-    }
+    { path: '/', component: Home },
+    { path: '*', redirect: '/' },
   ]
 })
