b85b68871317c6f57f449c27faa926c98bea7a9f
andy23512 <andy23512@gmail.com>
2018-07-28 00:41:54 +0800

    use promise to refine the code

--- ctare.js
+++ ctare.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 
-// load modules
 const child_process = require('child_process');
-const commandExistsSync = require('command-exists').sync;
+const commandExists = require('command-exists');
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
 const request = require('request');
 const url = require('url');
+
 const features = require('./lib/features');
+const depDeps = require('./lib/dev-deps');
+const fontUrls = require('./lib/font-urls');
+
+// global variables
 const files = {
   'main.js': {
     path: 'src/main.js',
@@ -23,82 +27,92 @@ const files = {
     toRemove: []
   }
 };
-
-// global variables
 const selectedFeatures = {};
-const fontsUrls = [
-  'https://raw.githubusercontent.com/minjiex/kaigen-gothic/master/dist/TW/KaiGenGothicTW-Light.ttf',
-  'https://raw.githubusercontent.com/minjiex/kaigen-gothic/master/dist/TW/KaiGenGothicTW-Regular.ttf',
-  'https://raw.githubusercontent.com/minjiex/kaigen-gothic/master/dist/TW/KaiGenGothicTW-Medium.ttf',
-  'https://raw.githubusercontent.com/minjiex/kaigen-gothic/master/dist/TW/KaiGenGothicTW-Bold.ttf'
-];
+let projectName = '';
 
-// get project name
-const projectName = process.argv[2];
-if (!projectName) {
-  console.log('Usage: ./ctare.js <project-name>');
-  process.exit(1);
-}
+getProjectName()
+  .then(() => commandExists('vue'))
+  .then(checkVueCliVersion)
+  .then(runVueCli)
+  .then(getCtareConfig)
+  .then(changeDirToProject)
+  .then(editBrowsersList)
+  .then(cloneCtareSource)
+  .then(copyFiles)
+  .then(handleFonts)
+  .then(handleModules)
+  .then(removeFiles)
+  .catch(err => console.error(err));
 
-// check if vue-cli is globally installed
-if (!commandExistsSync('vue')) {
-  console.log('Error: Vue-cli is not globally installed in this computer.');
-  console.log(
-    'Run "yarn global install @vue/cli" or "npm install --global @vue/cli" to install it.'
-  );
-  process.exit(1);
-}
-
-// check version of vue-cli
-const version = child_process.execSync('vue -V');
-const mainVersionNum = +version.toString().split('.')[0];
-if (mainVersionNum < 3) {
-  console.log('Error: The version of installed Vue-cli < 3.0.0');
-  console.log(
-    'Run "yarn global install @vue/cli" or "npm install --global @vue/cli" to install the latest vue-cli.'
-  );
-  process.exit(1);
-}
-
-// execute vue-cli
-child_process
-  .spawn('vue', ['create', projectName], { shell: true, stdio: 'inherit' })
-  .on('close', code => {
-    if (code !== 0) {
-      console.log('vue-cli process exited with code ' + code);
-    }
-    ctareConfig();
+function getProjectName() {
+  return new Promise((resolve, reject) => {
+    projectName = process.argv[2];
+    projectName
+      ? resolve(projectName)
+      : reject(
+          new Error(
+            'Error: No project name is given\nUsage: ./ctare.js <project-name>'
+          )
+        );
   });
+}
 
-function ctareConfig() {
-  // get features config
-  process.stdout.write('\033c\033[3J'); // clear screen
-  const choices = [
-    new inquirer.Separator(' = Fonts = '),
-    ...features.fonts,
-    new inquirer.Separator(' = Modules = '),
-    ...features.modules
-  ];
-  const questions = [
-    {
-      type: 'checkbox',
-      message: 'Check the features needed for your project: ',
-      name: 'features',
-      pageSize: 12,
-      choices
-    }
-  ];
-  inquirer.prompt(questions).then(answers => {
-    // parse selected features
-    answers['features'].forEach(c => {
-      selectedFeatures[c] = true;
+function checkVueCliVersion() {
+  return new Promise((resolve, reject) => {
+    child_process.exec('vue -V', (err, stdout, stderr) => {
+      const mainVersionNum = +stdout.toString().split('.')[0];
+      mainVersionNum >= 3
+        ? resolve()
+        : reject(
+            new Error(
+              'Error: The version of installed Vue-cli < 3.0.0\nRun "yarn global install @vue/cli" or "npm install --global @vue/cli" to install the latest vue-cli.'
+            )
+          );
     });
-
-    // change main directory to project
-    process.chdir(projectName);
-
-    installModules();
   });
+}
+
+function runVueCli() {
+  return promiseSpawn('vue', ['create', projectName]).catch(code => {
+    throw new Error('Vue-cli process exited with error code ' + code);
+  });
+}
+
+function getCtareConfig() {
+  process.stdout.write('\033c\033[3J'); // clear screen
+  return inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        message: 'Check the features needed for your project: ',
+        name: 'features',
+        pageSize: 12,
+        choices: [
+          new inquirer.Separator(' = Fonts = '),
+          ...features.fonts,
+          new inquirer.Separator(' = Modules = '),
+          ...features.modules
+        ]
+      }
+    ])
+    .then(answers => {
+      // parse selected features
+      answers['features'].forEach(c => {
+        selectedFeatures[c] = true;
+      });
+    });
+}
+
+function changeDirToProject() {
+  return Promise.resolve(process.chdir(projectName));
+}
+
+function installDeps(program) {
+  return (args, deps) => {
+    return promiseSpawn(program, [...args, ...deps]).catch(code => {
+      throw new Error('Dependencies install process exited with code ' + code);
+    });
+  };
 }
 
 function installModules() {
@@ -107,72 +121,45 @@ function installModules() {
   const installDepsProgram = hasYarn ? 'yarn' : 'npm';
   const installDepsArgs = hasYarn ? ['add'] : ['install', '--save'];
   const installDevDepsArgs = hasYarn ? ['add', '-D'] : ['install', '-D'];
-  const devDeps = [
-    'awesome-fontmin-loader',
-    'charactor-scanner',
-    'imagemin',
-    'imagemin-gifsicle',
-    'imagemin-mozjpeg',
-    'imagemin-pngquant',
-    'img-loader',
-    'pug',
-    'pug-plain-loader'
-  ];
+  const install = installDeps(installDepsProgram);
 
   // collect modules that need to installed
   const deps = features.modules
     .filter(m => selectedFeatures[m.name])
     .reduce((acc, m) => acc.concat(m.packages), []);
-  child_process
-    .spawn(installDepsProgram, [...installDepsArgs, ...deps], {
-      shell: true,
-      stdio: 'inherit'
-    })
-    .on('close', code => {
-      if (code !== 0) {
-        console.log('dependencies install process exited with code ' + code);
-      }
-      child_process
-        .spawn(installDepsProgram, [...installDevDepsArgs, ...devDeps], {
-          shell: true,
-          stdio: 'inherit'
-        })
-        .on('close', code => {
-          if (code !== 0) {
-            console.log(
-              'devDependencies install process exited with code ' + code
-            );
-          }
-          editBrowsersList();
-        });
-    });
+  return install(installDepsArgs, deps).then(
+    install(installDevDepsArgs, devDeps)
+  );
 }
 
 function editBrowsersList() {
-  let packageJson = JSON.parse(fs.readFileSync('package.json'));
-  packageJson.browserslist = ['> 1% in tw', 'last 3 versions', 'not ie <= 11'];
-  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
-  editGitIgnore();
-}
-
-function editGitIgnore() {
-  fs.appendFileSync('.gitignore', '\n# add by ctare\nsrc/assets/fonts/');
-  fetchFiles();
-}
-
-function fetchFiles() {
-  if (fs.existsSync('ctare-cli')) child_process.execSync('\\rm -rf ctare-cli/');
-  child_process
-    .spawn('git', ['clone', 'https://github.com/andy23512/ctare-cli/'], {
-      shell: true,
-      stdio: 'inherit'
-    })
-    .on('close', code => {
-      if (code !== 0) {
-        console.log('git clone process exited with code ' + code);
-      }
-      copyFiles();
+  return new Promise((resolve, reject) => {
+    fs.readFile('package.json', (err, data) => {
+      !err ? resolve(JSON.parse(data)) : reject(err);
     });
+  }).then(packageJson => {
+    packageJson.browserslist = [
+      '> 1% in tw',
+      'last 3 versions',
+      'not ie <= 11'
+    ];
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        'package.json',
+        JSON.stringify(packageJson, null, 2),
+        err => (!err ? resolve() : reject(err))
+      );
+    });
+  });
+}
+
+function cloneCtareSource() {
+  return promiseSpawn('git', [
+    'clone',
+    'https://github.com/andy23512/ctare-cli/'
+  ]).catch(code => {
+    throw new Error('Clone CTARE github repo exited with error code' + code);
+  });
 }
 
 function copyFiles() {
@@ -186,46 +173,51 @@ function copyFiles() {
   }
   if (!hasStore) {
     files['main.js'].toRemove.push('store');
-    handleFonts();
   }
+}
 
-  function handleFonts() {
-    if (selectedFeatures['Noto Sans TC']) {
-      if (!fs.existsSync('src/assets/fonts/'))
-        fs.mkdirSync('src/assets/fonts/');
-      fontsUrls.forEach(downloadFonts);
-    } else {
-      fs.unlinkSync('src/assets/font.sass');
-      files['global.sass'].toRemove.push('font');
-    }
-    handleModules();
+function handleFonts() {
+  if (selectedFeatures['Noto Sans TC']) {
+    fs.appendFileSync('.gitignore', '\n# add by ctare\nsrc/assets/fonts/');
+    if (!fs.existsSync('src/assets/fonts/')) fs.mkdirSync('src/assets/fonts/');
+    fontUrls.forEach(downloadFont);
+  } else {
+    fs.unlinkSync('src/assets/font.sass');
+    files['global.sass'].toRemove.push('font');
   }
+}
 
-  function downloadFonts(targetUrl) {
-    const fileName = path.basename(url.parse(targetUrl).pathname);
-    request(targetUrl).pipe(
-      fs.createWriteStream(path.join('src/assets/fonts/', fileName))
-    );
-  }
+function downloadFont(targetUrl) {
+  const fileName = path.basename(url.parse(targetUrl).pathname);
+  request(targetUrl).pipe(
+    fs.createWriteStream(path.join('src/assets/fonts/', fileName))
+  );
+}
 
-  function handleModules() {
-    features.modules.forEach(f => {
-      if(selectedFeatures[f.name] || !f.affectedFile) return;
-      files[f.affectedFile].toRemove.push(f.name)
-    })
-    removeFiles()
-  }
+function handleModules() {
+  features.modules.forEach(f => {
+    if (selectedFeatures[f.name] || !f.affectedFile) return;
+    files[f.affectedFile].toRemove.push(f.name);
+  });
+}
 
-  function removeFiles() {
-    Object.values(files).forEach(f => {
-      let fileContent = fs.readFileSync(f.path, { encoding: 'utf8' });
-      for (const t of f.toRemove)
-        fileContent = fileContent.replace(
-          new RegExp(`\\n.*\\[\\[${t}\\]\\]`, 'g'),
-          ''
-        );
-      fileContent = fileContent.replace(/ \/\/\[\[.*$/gm, '');
-      fs.writeFileSync(f.path, fileContent);
-    });
-  }
+function removeFiles() {
+  Object.values(files).forEach(f => {
+    let fileContent = fs.readFileSync(f.path, { encoding: 'utf8' });
+    for (const t of f.toRemove)
+      fileContent = fileContent.replace(
+        new RegExp(`\\n.*\\[\\[${t}\\]\\]`, 'g'),
+        ''
+      );
+    fileContent = fileContent.replace(/ \/\/\[\[.*$/gm, '');
+    fs.writeFileSync(f.path, fileContent);
+  });
+}
+
+function promiseSpawn(command, args) {
+  return new Promise((resolve, reject) => {
+    child_process
+      .spawn(command, args, { shell: true, stdio: 'inherit' })
+      .on('close', code => (code == 0 ? resolve() : reject()));
+  });
 }
