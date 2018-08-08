65c4c4607197444345d2ac5a216ba70634adffc2
andy23512 <andy23512@gmail.com>
2018-08-08 14:42:28 +0800

    add vue2-hammer feature

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import axios from 'axios' //[[axios]]
 import VueRx from 'vue-rx' //[[rxjs]]
 import VueAnalytics from 'vue-analytics' //[[vue-analytics]]
 import VueScrollTo from 'vue-scrollto' //[[vue-scrollto]]
+import { VueHammer } from 'vue2-hammer' //[[vue2-hammer]]
 
 import App from '@/App.vue'
 import router from '@/router' //[[router]]
@@ -14,6 +15,7 @@ import store from '@/store' //[[store]]
 Vue.use(VueRx) //[[rxjs]]
 Vue.use(VueAnalytics, { id: 'UA-XXX-X' }) //[[vue-analytics]]
 Vue.use(VueScrollTo) //[[vue-scrollto]]
+Vue.use(VueHammer) //[[vue2-hammer]]
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios //[[axios]]
