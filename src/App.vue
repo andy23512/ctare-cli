5f69394e6dc27d0111030ce80a33f16201fb82af
andy23512 <andy23512@gmail.com>
2018-08-21 16:10:15 +0800

    add preset App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+#v-app.app App
+</template>
+
+<script>
+import query from 'query-string'
+
+export default {
+  name: 'App',
+  created() {
+    $(window).resize(() => this.$store.commit('checkMobile')).resize()
+    this.$store.commit('saveUtm', query.parse(location.search))
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
