9c6fd2ac1a03c62e3f74d739c073475b13c2782f
Tangent Chang <andy23512@gmail.com>
2019-01-26 18:47:06 +0800

    change import order in main.js

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ import 'normalize.css/normalize.css' //[[normalize.css]]
 import '@/global.sass'
 
 import Vue from 'vue'
-import axios from 'axios' //[[axios]]
-import VueRx from 'vue-rx' //[[rxjs]]
 import VueAnalytics from 'vue-analytics' //[[vue-analytics]]
+import VueRx from 'vue-rx' //[[rxjs]]
 import VueScrollTo from 'vue-scrollto' //[[vue-scrollto]]
+import axios from 'axios' //[[axios]]
 
 import App from '@/App.vue'
 import router from '@/router' //[[router]]
 import store from '@/store' //[[store]]
 
-Vue.use(VueRx) //[[rxjs]]
 Vue.use(VueAnalytics, { id: 'UA-XXX-X' }) //[[vue-analytics]]
+Vue.use(VueRx) //[[rxjs]]
 Vue.use(VueScrollTo) //[[vue-scrollto]]
 
 Vue.config.productionTip = false
