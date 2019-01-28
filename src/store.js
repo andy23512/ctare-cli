f3ce144e3444aab1e6568d95d19b13665cfc2e88
Tangent Chang <andy23512@gmail.com>
2019-01-28 22:29:43 +0800

    add utm layer to utm state

--- src/store.js
+++ src/store.js
@@ -6,11 +6,13 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     mobile: true, //[[Check Mobile]]
-    utm_source: '', //[[Save UTM]]
-    utm_medium: '', //[[Save UTM]]
-    utm_campaign: '', //[[Save UTM]]
-    utm_term: '', //[[Save UTM]]
-    utm_content: '', //[[Save UTM]]
+    utm: { //[[Save UTM]]
+      utm_source: '', //[[Save UTM]]
+      utm_medium: '', //[[Save UTM]]
+      utm_campaign: '', //[[Save UTM]]
+      utm_term: '', //[[Save UTM]]
+      utm_content: '', //[[Save UTM]]
+    }, //[[Save UTM]]
   },
   mutations: {
     checkMobile(state) { //[[Check Mobile]]
@@ -18,11 +20,11 @@ export default new Vuex.Store({
       state.mobile = window.innerWidth < 992 //[[Check Mobile,!jquery]]
     }, //[[Check Mobile]]
     saveUtm(state, query) { //[[Save UTM]]
-      state.utm_source = query.utm_source ? query.utm_source : '' //[[Save UTM]]
-      state.utm_medium = query.utm_medium ? query.utm_medium : '' //[[Save UTM]]
-      state.utm_campaign = query.utm_campaign ? query.utm_campaign : '' //[[Save UTM]]
-      state.utm_term = query.utm_term ? query.utm_term : '' //[[Save UTM]]
-      state.utm_content = query.utm_content ? query.utm_content : '' //[[Save UTM]]
+      state.utm.utm_source = query.utm_source ? query.utm_source : '' //[[Save UTM]]
+      state.utm.utm_medium = query.utm_medium ? query.utm_medium : '' //[[Save UTM]]
+      state.utm.utm_campaign = query.utm_campaign ? query.utm_campaign : '' //[[Save UTM]]
+      state.utm.utm_term = query.utm_term ? query.utm_term : '' //[[Save UTM]]
+      state.utm.utm_content = query.utm_content ? query.utm_content : '' //[[Save UTM]]
     }, //[[Save UTM]]
   },
   actions: {
