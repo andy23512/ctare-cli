d17ff7412d45cd3550f5013c10122604d39219eb
Tangent Chang <andy23512@gmail.com>
2019-02-25 17:08:59 +0800

    fix bug in ctare.js after moving font.sass

--- ctare.js
+++ ctare.js
@@ -229,12 +229,13 @@ function editBrowsersList() {
 function handleFonts() {
   if (selectedFeatures['Noto Sans TC']) {
     fs.appendFileSync('.gitignore', '\n# add by ctare\nsrc/assets/fonts');
+    if (!fs.existsSync('src/assets/')) fs.mkdirSync('src/assets/');
     if (!fs.existsSync('src/assets/fonts/')) fs.mkdirSync('src/assets/fonts/');
     fontUrls.forEach(downloadFont);
     files['global.sass'].toRemove.push('!font');
     files['vue.config.js'].toRemove.push('!font');
   } else {
-    fs.unlinkSync('src/assets/font.sass');
+    fs.unlinkSync('src/font.sass');
     files['global.sass'].toRemove.push('font');
     files['vue.config.js'].toRemove.push('font');
   }
