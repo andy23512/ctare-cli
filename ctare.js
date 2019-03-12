459b871459f883ca7afec26f3938c80f823f8a6c
Tangent Chang <andy23512@gmail.com>
2019-03-12 19:06:03 +0800

    Revert "Remove Noto support"
    This reverts commit c3e607c35d1771403f612f8d1cb79dd81d6c58ae.

--- ctare.js
+++ ctare.js
@@ -11,6 +11,7 @@ const url = require('url');
 
 const features = require('./lib/features');
 const fixedDevDeps = require('./lib/dev-deps');
+const fontUrls = require('./lib/font-urls');
 
 // global variables
 const files = {
@@ -57,6 +58,7 @@ getProjectName()
   .then(copyFiles)
   .then(installModules)
   .then(editBrowsersList)
+  .then(handleFonts)
   .then(handleDistIgnore)
   .then(handleModules)
   .then(handleComplexToRemove)
@@ -121,6 +123,8 @@ function getCtareConfig() {
         name: 'features',
         pageSize: 20,
         choices: [
+          new inquirer.Separator(' = Fonts = '),
+          ...features.fonts,
           ...selectableFunctions,
           new inquirer.Separator(' = Modules = '),
           ...features.modules
@@ -198,6 +202,7 @@ function installModules() {
   // collect modules that need to installed
   const installed = features.modules
     .concat(features.functions)
+    .concat(features.fonts)
     .filter(m => selectedFeatures[m.name])
   const deps = installed.reduce((acc, m) => acc.concat(m.packages), []);
   const devDeps = installed.reduce((acc, m) => acc.concat(m.devPackages), fixedDevDeps);
@@ -219,6 +224,28 @@ function editBrowsersList() {
       err => (!err ? resolve() : reject(err))
     );
   });
+}
+
+function handleFonts() {
+  if (selectedFeatures['Noto Sans TC']) {
+    fs.appendFileSync('.gitignore', '\n# add by ctare\nsrc/assets/font');
+    if (!fs.existsSync('src/assets/')) fs.mkdirSync('src/assets/');
+    if (!fs.existsSync('src/assets/font/')) fs.mkdirSync('src/assets/font/');
+    fontUrls.forEach(downloadFont);
+    files['global.sass'].toRemove.push('!font');
+    files['vue.config.js'].toRemove.push('!font');
+  } else {
+    fs.unlinkSync('src/font.sass');
+    files['global.sass'].toRemove.push('font');
+    files['vue.config.js'].toRemove.push('font');
+  }
+}
+
+function downloadFont(targetUrl) {
+  const fileName = path.basename(url.parse(targetUrl).pathname);
+  request(targetUrl).pipe(
+    fs.createWriteStream(path.join('src/assets/font/', fileName))
+  );
 }
 
 function handleDistIgnore() {
