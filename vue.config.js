6061d165784c2872dd716d206ccf62870cca42d1
Tangent Chang <andy23512@gmail.com>
2019-09-17 13:34:22 +0800

    Remove protocol in target of vue config

--- vue.config.js
+++ vue.config.js
@@ -40,7 +40,7 @@ module.exports = {
     hot: false,
     inline: true,
     port: process.env.DEV_PORT || 8080,
-    proxy: { '/api': { target: `http://${process.env.API_HOST || 'localhost'}:${process.env.API_PORT || 8000}` } },
+    proxy: { '/api': { target: `${process.env.API_HOST || 'localhost'}:${process.env.API_PORT || 8000}` } },
   },
   //<<jquery@module&storybook@plugin>>
   pluginOptions: {
