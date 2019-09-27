03c8fd70d514a81b7b7d9e10c0ad0647727229b1
Tangent Chang <andy23512@gmail.com>
2019-09-27 14:04:37 +0800

    Set default state of mobile to null

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    mobile: false, //[[check-mobile@function]]
+    mobile: null, //[[check-mobile@function]]
     //<<save-utm@function>>
     utm: {
       utm_source: null,
