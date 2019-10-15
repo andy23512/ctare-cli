edeecced3d166d1c15f83429a314d4d7eb3f2e2b
andy23512 <andy23512@gmail.com>
2019-10-15 14:31:15 +0800

    Rename Home to VHome in router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '@/views/Home.vue'
+import VHome from '@/views/Home.vue'
 
 Vue.use(Router)
 
@@ -16,7 +16,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Home,
+      component: VHome,
     },
     {
       path: '*',
