1c9dabf0570e2e26d298cb8478b558f687741180
andy23512 <andy23512@gmail.com>
2019-11-18 16:16:45 +0800

    setup

---
+++ nanoha/src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+#v-app
+  | App
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="sass" scoped>
+@import "~@/mixin.sass"
+
+#v-app
+  min-height: 100%
+  min-width: 375px
+</style>
