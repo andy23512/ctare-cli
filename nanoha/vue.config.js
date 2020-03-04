9da69107f7dae2158484f449c964812f25d940de
andy23512 <andy23512@gmail.com>
2020-03-04 14:25:44 +0800

    setup

--- nanoha/vue.config.js
+++ nanoha/vue.config.js
@@ -12,6 +12,24 @@ module.exports = {
       }])
 
     if(config.get('mode') === 'production') {
+      config.module.rules.delete('images')
+      config.module.rule('images')
+        .test(/\.(png|jpe?g|gif)(\?.*)?$/)
+        .use('file-loader')
+          .loader('file-loader')
+          .options({
+            name: `img/[name].[hash:8].[ext]`
+          })
+          .end()
+        .use('img-loader')
+          .loader('img-loader')
+          .options({
+            plugins: [
+              require('imagemin-gifsicle')(),
+              require('imagemin-mozjpeg')(),
+              require('imagemin-pngquant')(),
+            ]
+          })
     }
   },
   devServer: {
