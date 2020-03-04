ea9b421ee226f4261508323a773c33a808b70541
andy23512 <andy23512@gmail.com>
2020-03-04 13:57:29 +0800

    setup

---
+++ nanoha/src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+#v-app: .container
+  | App
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="sass" scoped>
+@import "~@/mixin.sass"
+
+#v-app
+  background-color: white
+  margin-left: auto
+  margin-right: auto
+  max-width: 1280px
+  min-height: 100%
+  min-width: 375px
+
+@media (max-width: 1019px)
+  #v-app
+    max-width: 600px
+</style>
